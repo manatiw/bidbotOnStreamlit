de1ed4ac1e3b4ff4b207ae025652190efcec85de
manatiw <62131532+manatiw@users.noreply.github.com>
2025-02-04 17:15:20 +0800

    UI layout first draft

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import streamlit as sl
 
 sl.title("Moldev相關政府採購標案下載")
+sl.markdown("---")
+
+sd=sl.date_input("開始日期")
+
+tk=sl.text_area("title kw")
+
+
+ck=sl.text_area("company kw")
+sl.checkbox("AI選擇相關標案", value=True)
+
+sl.button("完成設定")
+
+
+bar=sl.progress(0)
+
+sl.button("下載csv")
